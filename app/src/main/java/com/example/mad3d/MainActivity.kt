13b1d619640076bb0e2fbb9dba6fd324faf22a0e
Hoffinn <k.hoffmannova@post.cz>
2024-06-04 12:37:54 +0200

    options menu

--- app/src/main/java/com/example/mad3d/MainActivity.kt
+++ app/src/main/java/com/example/mad3d/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.mad3d
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import com.example.mad3d.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationBarView.OnItemSelectedListener
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnItemSelectedListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -15,27 +18,53 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonMap.setOnClickListener { onMapClicked() }
-        binding.buttonExplore.setOnClickListener { onExploreClicked() }
-        binding.buttonAR.setOnClickListener { onARClicked() }
+        binding.bottomNav.setOnItemSelectedListener(this)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.download_POIs -> {
+            true
         }
 
-        private fun onMapClicked() {
-            supportFragmentManager.commit {
-                replace(R.id.frame_content, MapFragment())
-            }
+        R.id.delete_POIs -> {
+            true
         }
 
-        private fun onExploreClicked() {
-            supportFragmentManager.commit {
-                replace(R.id.frame_content, ExploreFragment())
-            }
+        else -> {
+            super.onOptionsItemSelected(item)
         }
+    }
 
-        private fun onARClicked() {
-            supportFragmentManager.commit {
-                replace(R.id.frame_content, ARFragment())
-            }
+    private fun onMapClicked(): Boolean {
+        supportFragmentManager.commit {
+            replace(R.id.frame_content, MapFragment())
         }
+        return true
+    }
+
+    private fun onExploreClicked(): Boolean {
+        supportFragmentManager.commit {
+            replace(R.id.frame_content, ExploreFragment())
+        }
+        return true
+    }
+
+    private fun onARClicked(): Boolean {
+        supportFragmentManager.commit {
+            replace(R.id.frame_content, ARFragment())
+        }
+        return true
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.nav_explore -> onExploreClicked()
+        R.id.nav_map -> onMapClicked()
+        R.id.nav_ar -> onARClicked()
+        else -> false
+    }
 }
